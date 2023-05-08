dea2232a3d91856d4967da47781a13c06ff70caf
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-05-08 11:49:42 -0500

    changes made to servlets-exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,9 +10,20 @@ import java.io.PrintWriter;
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        String name = req.getParameter("name");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        if (name != null) {
+            out.println("<h3>Hello " + name + "!</h3>");
+        } else {
+            out.println("<h3>Hello, World!</h3>");
+        }
     }
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//        res.setContentType("text/html");
+//        PrintWriter out = res.getWriter();
+//        out.println("<h1>Hello, World!</h1>");
+//    }
 }
