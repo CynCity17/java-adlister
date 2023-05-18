c5f04e08c5f3efe0cf7042ed1fc429d72cf02b25
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-05-18 09:52:15 -0500

    adding minor changes

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,8 +1,18 @@
 package com.codeup.adlister.dao;
 
+import config.Config;
+
 public class DaoFactory {
     private static Ads adsDao;
+    private static Users usersDao;
     private static Config config = new Config();
+
+    public static Users getUserDao(){
+        if (usersDao == null){
+            usersDao = new MySQLUsersDao(config);
+        }
+        return usersDao;
+    }
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
